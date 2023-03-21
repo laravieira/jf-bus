7241a1742477da4b26eb066626fa7d82520e271d
Lara Vieira <contato@laravieira.me>
2023-03-21 18:02:06 -0300

    Added Inter font and async font loading.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,29 @@
 import Navigation from './src/navigation/Navigation.component';
+import { Inter_300Light, Inter_500Medium } from '@expo-google-fonts/inter';
+import { preventAutoHideAsync, hideAsync } from 'expo-splash-screen';
+import { useEffect, useState } from 'react';
+import { loadAsync } from 'expo-font';
+
+preventAutoHideAsync().then();
 
 function App() {
+  const [appReady, setAppReady] = useState<boolean>(false);
+
+  useEffect(() => {
+    loadAsync({
+      Inter_300Light,
+      Inter_500Medium,
+    }).catch(console.warn)
+      .finally(() => setAppReady(true));
+  }, []);
+
+  useEffect(() => {
+    if(appReady)
+      hideAsync().then();
+  }, [appReady]);
+
+  if(!appReady)
+    return null;
   return <Navigation/>;
 }
 
